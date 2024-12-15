42991ed36d68d8374977a0a04a49ed9276919b9c
Paulius Greičiūnas <paulius.greiciunas@barbora.lt>
2024-12-15 23:01:31 +0200

    chore: add getAtomUrl to AtomPerson

--- lib/core/extensions.dart
+++ lib/core/extensions.dart
@@ -1,8 +1,17 @@
+import 'package:webfeed_revised/domain/atom_person.dart';
+
 extension CapitalizeFirstLetter on String {
   String capitalizeFirst() {
     if (isEmpty) {
       return this;
     }
     return '${this[0].toUpperCase()}${substring(1).toLowerCase()}';
+  }
+}
+
+extension UrlToAtom on AtomPerson {
+  String? getAtomUrl() {
+    if (uri == null) return null;
+    return uri!.endsWith('.atom') ? uri : '$uri.atom';
   }
 }
